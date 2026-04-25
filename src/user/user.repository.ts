9268f70b6dbf4fc1476930e454cd7237ac7b4b0a
Nobsmoke123 <donaldakobundu@gmail.com>
2026-04-25 18:41:16 +0100

    Remove comments

--- src/user/user.repository.ts
+++ src/user/user.repository.ts
@@ -9,43 +9,4 @@ export class UserRepository extends EntityRepository<UserDocument> {
   constructor(@InjectModel(User.name) userModel: Model<UserDocument>) {
     super(userModel);
   }
-
-  // async findAll(usersFilterQuery: QueryFilter<User>): Promise<User[]> {
-  //   return this.userModel.find(usersFilterQuery);
-  // }
-
-  // async findOne(userFilterQuery: QueryFilter<User>): Promise<User> {
-  //   const user = await this.userModel.findOne(userFilterQuery);
-  //   if (!user) {
-  //     throw new NotFoundException('User not found.');
-  //   }
-  //   return user;
-  // }
-
-  // async create(user: CreateUserDto): Promise<User> {
-  //   const newUser = await this.userModel.create(user);
-  //   return newUser.save();
-  // }
-
-  // async update(
-  //   userFilterQuery: QueryFilter<User>,
-  //   userUpdate: Partial<User>,
-  // ): Promise<User> {
-  //   const user = await this.userModel.findOneAndUpdate(
-  //     userFilterQuery,
-  //     userUpdate,
-  //   );
-  //   if (!user) {
-  //     throw new NotFoundException('User not found.');
-  //   }
-  //   return user;
-  // }
-
-  // async remove(userFilterQuery: QueryFilter<User>): Promise<User> {
-  //   const user = await this.userModel.findOneAndDelete(userFilterQuery);
-  //   if (!user) {
-  //     throw new NotFoundException('User not found.');
-  //   }
-  //   return user;
-  // }
 }
